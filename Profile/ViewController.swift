fe1425489384a40a1ed27cc3e190e15ed153ecf5
O-suke12 <souke0504@gmail.com>
2020-05-10 15:25:48 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by RS on 2020/05/10.
+//  Copyright © 2020 osuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
