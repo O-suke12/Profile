3466e7457f56b6deb055597319aaea7cc4c65c51
O-suke12 <souke0504@gmail.com>
2020-05-10 16:23:28 +0900

    発展

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -10,19 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var profile: UIImageView!
+    @IBOutlet var label:UILabel!
     @IBOutlet var comment: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        profile.isHidden = true
-        comment.isHidden = true
+     
         
     }
-    @IBAction func tap() {
-        profile.isHidden = false
-        comment.isHidden = false
+    @IBAction func tap1() {
+        profile.image = UIImage(named:"phil_image")
+        label.text = "名前"
+        comment.text = "iPhoneメンターのフィルだよ！"
         
     }
+    @IBAction func tap2() {
+           profile.image = UIImage(named:"apple_image")
+           label.text = "好きな食べ物"
+           comment.text = "リンゴが好きでいつも持ち歩いている"
+           
+       }
+    @IBAction func tap3() {
+           profile.image = UIImage(named:"track_image")
+           label.text = "スポーツ"
+           comment.text = "陸上競技が好きで、走り幅跳びが得意"
+           
+       }
+    @IBAction func tap4() {
+           profile.image = UIImage(named:"flight_image")
+           label.text = "趣味"
+           comment.text = "飛行機に乗って空を散歩すること"
+           
+       }
 
+    
 }
 
